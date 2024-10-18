1feca2413dd8bd6b5286a2f71164963b07dc352a
Alex Seifert <alex@alexseifert.com>
2024-10-18 11:08:12 +0200

    Add the repo for Haunting Alex

--- src/index.mjs
+++ src/index.mjs
@@ -4,7 +4,7 @@ import { WordPressExporter } from './exporter.mjs';
 
 const blogUrls = [
   ['alexsnotebook', 'https://blog.alexseifert.com/wp-json/wp/v2/', 'blog-archive-alexsnotebook'],
-  // ['hauntingalex', 'https://haunting.alexseifert.com/wp-json/wp/v2/', ''],
+  ['hauntingalex', 'https://haunting.alexseifert.com/wp-json/wp/v2/', 'blog-archive-hauntingalex'],
   // ['historyrhymes', 'https://www.historyrhymes.info/wp-json/wp/v2/', ''],
   // ['thebeskirtedman', 'https://www.the-beskirted-man.com/wp-json/wp/v2/', ''],
 ];
